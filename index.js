2102f5a4f8e5eb5b9ba122c99a70a24fae037213
Nesgc <nestorgarciac11@gmail.com>
2024-06-19 22:03:05 -0600

    product model

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, () =>{
-    console.log('server running');
-});
+
 
 app.get('/', (req, res)=>{
     res.send("hello from node apis");
 });
 
-mongoose.connect("mongodb+srv://nesgc:<password>@backenddb.ex3qwzv.mongodb.net/?retryWrites=true&w=majority&appName=Backenddb")
+mongoose.connect("mongodb+srv://nesgc:1fPBCHuA1MEPsZAt@backenddb.ex3qwzv.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Backenddb")
+.then(()=>{
+    console.log("Connected to database");
+    app.listen(3000, () =>{
+        console.log('server running on port 3000');
+    });
+})
+.catch(()=>{
+    console.log("Conn failed");
+});
